d29236762a3235f198ada24c915251ae3e23236d
Serhii Babanov <serhii.babanov@gmail.com>
2024-03-09 10:00:32 -0500

    first

--- src/main/java/ua/flowerista/shop/configs/OpenApiConfig.java
+++ src/main/java/ua/flowerista/shop/configs/OpenApiConfig.java
@@ -32,7 +32,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 ),
                 @Server(
                         description = "PROD ENV",
-                        url = "https://floverista-011daa2eb6c3.herokuapp.com/"
+                        url = "https://notes-sec-afa34825506c.herokuapp.com/"
                 )
         },
         security = {
