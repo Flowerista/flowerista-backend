eddac3d5246d9336f22a89d51e970f0c4cf0dd19
Serhii Babanov <serhii.babanov@gmail.com>
2024-03-20 22:44:43 -0400

    add objects to orderView template

--- src/main/java/ua/flowerista/shop/controllers/AdminPanelController.java
+++ src/main/java/ua/flowerista/shop/controllers/AdminPanelController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ua.flowerista.shop.dto.OrderDto;
-import ua.flowerista.shop.dto.user.UserDto;
 import ua.flowerista.shop.mappers.OrderMapper;
 import ua.flowerista.shop.models.Order;
 import ua.flowerista.shop.models.OrderStatus;
@@ -39,9 +38,11 @@ public class AdminPanelController {
 
     @GetMapping("/orders/{id}")
     public ModelAndView getById(@PathVariable Integer id) {
+        ModelAndView result = new ModelAndView("admin/orders/orderView");
         OrderDto order = orderMapper.toDto(orderService.getOrder(id).orElseThrow());
-        UserDto user = order.getUser();
-        return new ModelAndView("admin/orders/orderView").addObject("order", order);
+        result.addObject("order", order);
+        result.addObject("statuses", OrderStatus.values());
+        return result;
     }
 
     @PostMapping("/orders/{id}/status")
