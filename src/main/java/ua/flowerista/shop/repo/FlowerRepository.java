a3e820f6d26b55645911b7609bffb49cbc3262f9
Mafia0331 <J.K.play2015@gmail.com>
2024-03-26 20:48:01 +0200

    add methods to retrieve a page with data

--- src/main/java/ua/flowerista/shop/repo/FlowerRepository.java
+++ src/main/java/ua/flowerista/shop/repo/FlowerRepository.java
@@ -1,11 +1,21 @@
 package ua.flowerista.shop.repo;
 
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
 import org.springframework.stereotype.Repository;
 
 import ua.flowerista.shop.models.Flower;
+import ua.flowerista.shop.models.QFlower;
 
 @Repository
-public interface FlowerRepository extends JpaRepository<Flower, Integer>  {
+public interface FlowerRepository extends JpaRepository<Flower, Integer>, QuerydslPredicateExecutor<Flower>, QuerydslBinderCustomizer<QFlower>{
 
+    @Override
+    default void customize(QuerydslBindings bindings, QFlower root) {
+        bindings.bind(String.class).first((SingleValueBinding<StringPath, String>) (path, s) -> path.equalsIgnoreCase(s));
+    }
 }
