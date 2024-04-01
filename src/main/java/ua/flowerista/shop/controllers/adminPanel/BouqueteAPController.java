cdc54db052879a6c94ab509e4a3de9f035b2543c
Mafia0331 <J.K.play2015@gmail.com>
2024-04-01 14:52:28 +0300

    create controller for Bouquet in admin panel

---
+++ src/main/java/ua/flowerista/shop/controllers/adminPanel/BouqueteAPController.java
@@ -0,0 +1,42 @@
+package ua.flowerista.shop.controllers.adminPanel;
+
+import com.querydsl.core.types.Predicate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import ua.flowerista.shop.dto.BouqueteDto;
+import ua.flowerista.shop.mappers.BouqueteMapper;
+import ua.flowerista.shop.models.Bouquete;
+import ua.flowerista.shop.services.BouqueteService;
+
+@Controller
+@RequestMapping("/api/admin/bouquets")
+@RequiredArgsConstructor
+public class BouqueteAPController {
+    private final BouqueteService bouqueteService;
+    private final BouqueteMapper bouqueteMapper;
+
+    @GetMapping
+    public ModelAndView getBouqets(@QuerydslPredicate(root = Bouquete.class) Predicate predicate,
+                                   @RequestParam(name = "page", defaultValue = "0", required = false) Integer page,
+                                   @RequestParam(name = "size", defaultValue = "10", required = false) Integer size,
+                                   @RequestParam(name = "search", required = false) String searchQuery) {
+        Page<BouqueteDto> bouquets;
+        if (searchQuery != null && !searchQuery.isEmpty()) {
+            bouquets = bouqueteService.searchBouquetsByName(searchQuery, PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id")))
+                    .map(bouqueteMapper::toDto);
+        } else {
+            bouquets = bouqueteService.getAllBouquetes(predicate, PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id")))
+                    .map(bouqueteMapper::toDto);
+        }
+        return new ModelAndView("admin/bouquets/bouquetList").addObject("bouquets", bouquets);
+    }
+
+}
