34350106d28ab4999c590d7f74ceb27736a24b5a
Mafia0331 <J.K.play2015@gmail.com>
2024-04-10 20:10:04 +0300

    add methods for get one bouquet and delete image from bouquet

--- src/main/java/ua/flowerista/shop/services/BouqueteService.java
+++ src/main/java/ua/flowerista/shop/services/BouqueteService.java
@@ -27,68 +27,73 @@ import java.util.stream.Collectors;
 @Transactional
 public class BouqueteService {
 
-	private BouqueteRepository repo;
-	private BouqueteMapper mapper;
-	private CloudinaryService cloudinary;
+    private BouqueteRepository repo;
+    private BouqueteMapper mapper;
+    private CloudinaryService cloudinary;
 
-	@Autowired
-	public BouqueteService(BouqueteRepository repo, BouqueteMapper mapper, CloudinaryService cloudinary) {
-		this.repo = repo;
-		this.mapper = mapper;
-		this.cloudinary = cloudinary;
-	}
+    @Autowired
+    public BouqueteService(BouqueteRepository repo, BouqueteMapper mapper, CloudinaryService cloudinary) {
+        this.repo = repo;
+        this.mapper = mapper;
+        this.cloudinary = cloudinary;
+    }
 
-	public void insert(BouqueteDto dto, List<MultipartFile> images) {
-		Map<Integer, String> imageUrls = new HashMap<>();
-		for (int i = 0; i < images.size(); i++) {
-			 MultipartFile image = images.get(i);
-			 String imageUrl = null;
-			 try {
-				imageUrl = cloudinary.uploadImage(image);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			 imageUrls.put(++i, imageUrl);
-		}
-		dto.setImageUrls(imageUrls);
-		repo.save(mapper.toEntity(dto));
-	}
+    public void insert(BouqueteDto dto, List<MultipartFile> images) {
+        Map<Integer, String> imageUrls = dto.getImageUrls() == null ? new HashMap<Integer, String>() : dto.getImageUrls();
+        int lastIndex = imageUrls.keySet().stream().max(Integer::compareTo).orElse(0);
+        String imageUrl;
+        for (int i = 0; i < images.size(); i++) {
+            imageUrl = null;
+            try {
+                imageUrl = cloudinary.uploadImage(images.get(i));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            imageUrls.put(lastIndex + i + 1, imageUrl);
+        }
+        dto.setImageUrls(imageUrls);
+        repo.save(mapper.toEntity(dto));
+    }
 
-	public void insert(BouqueteDto dto) {
-		repo.save(mapper.toEntity(dto));
-	}
+    public void insert(BouqueteDto dto) {
+        repo.save(mapper.toEntity(dto));
+    }
 
-	public void deleteById(int id) {
-		repo.deleteById(id);
-	}
+    public void deleteById(int id) {
+        repo.deleteById(id);
+    }
 
-	public List<BouqueteDto> getAllBouquetes() {
-		return repo.findAll().stream().map(bouquete -> mapper.toDto(bouquete)).collect(Collectors.toList());
-	}
+    public List<BouqueteDto> getAllBouquetes() {
+        return repo.findAll().stream().map(bouquete -> mapper.toDto(bouquete)).collect(Collectors.toList());
+    }
 
-	public BouqueteDto getBouqueteById(int id) {
-		return mapper.toDto(repo.getReferenceById(id));
-	}
+    public BouqueteDto getBouqueteById(int id) {
+        return mapper.toDto(repo.getReferenceById(id));
+    }
 
-	public void update(BouqueteDto dto) {
-		repo.save(mapper.toEntity(dto));
-	}
+    public Bouquete getBouquet(int id) {
+        return repo.findById(id);
+    }
 
-	public List<BouqueteSmallDto> getBouquetesBestSellers() {
-		return repo.findTop5ByOrderBySoldQuantityDesc().stream().map(bouquete -> mapper.toSmallDto(bouquete))
-				.collect(Collectors.toList());
-	}
+    public void update(BouqueteDto dto) {
+        repo.save(mapper.toEntity(dto));
+    }
 
-	public List<BouqueteSmallDto> getBouquetesTop5Sales() {
-		return repo.findTop5ByOrderByDiscountDesc().stream().map(bouquete -> mapper.toSmallDto(bouquete))
-				.collect(Collectors.toList());
-	}
+    public List<BouqueteSmallDto> getBouquetesBestSellers() {
+        return repo.findTop5ByOrderBySoldQuantityDesc().stream().map(bouquete -> mapper.toSmallDto(bouquete))
+                .collect(Collectors.toList());
+    }
 
-	public Page<BouqueteSmallDto> getBouquetesCatalogFiltered (List<Integer> flowerIds, List<Integer> colorIds, Integer minPrice, Integer maxPrice, Boolean sortByNewest,Boolean sortByPriceHighToLow,Boolean sortByPriceLowToHigh, int page) {
-		Pageable pageable = PageRequest.of(page - 1, 20);
-		return repo.findByFilters(flowerIds, colorIds, minPrice, maxPrice, sortByNewest, sortByPriceHighToLow, sortByPriceLowToHigh, pageable).map(mapper::toSmallDto);
+    public List<BouqueteSmallDto> getBouquetesTop5Sales() {
+        return repo.findTop5ByOrderByDiscountDesc().stream().map(bouquete -> mapper.toSmallDto(bouquete))
+                .collect(Collectors.toList());
+    }
 
-	}
+    public Page<BouqueteSmallDto> getBouquetesCatalogFiltered(List<Integer> flowerIds, List<Integer> colorIds, Integer minPrice, Integer maxPrice, Boolean sortByNewest, Boolean sortByPriceHighToLow, Boolean sortByPriceLowToHigh, int page) {
+        Pageable pageable = PageRequest.of(page - 1, 20);
+        return repo.findByFilters(flowerIds, colorIds, minPrice, maxPrice, sortByNewest, sortByPriceHighToLow, sortByPriceLowToHigh, pageable).map(mapper::toSmallDto);
+
+    }
 
     public PriceRangeDto getMinMaxPrices() {
         Integer minPrice = repo.findMinPrice();
@@ -97,8 +102,8 @@ public class BouqueteService {
         return new PriceRangeDto(minPrice, maxPrice);
     }
 
-    public BouqueteCardDto getById (int id) {
-    	return mapper.toCardDto(repo.getReferenceById(id));
+    public BouqueteCardDto getById(int id) {
+        return mapper.toCardDto(repo.getReferenceById(id));
     }
 
     public List<BouqueteSmallDto> searchBouquetesByName(String name) {
@@ -108,44 +113,52 @@ public class BouqueteService {
         return repo.searchByName(name).stream().map(boquete -> mapper.toSmallDto(boquete)).collect(Collectors.toList());
     }
 
-	public Boolean isBouqueteExist(Integer id) {
-		return repo.existsById(id);
-	}
-
-    public void test () {
-    	Bouquete bouquete = new Bouquete();
-    	BouqueteSize sizeS = new BouqueteSize();
-    	sizeS.setSize(Size.SMALL);
-    	sizeS.setDefaultPrice(BigInteger.valueOf(123));
-    	BouqueteSize sizeM = new BouqueteSize();
-    	sizeM.setSize(Size.MEDIUM);
-    	sizeM.setDefaultPrice(BigInteger.valueOf(140));
-    	BouqueteSize sizeL = new BouqueteSize();
-    	sizeL.setSize(Size.LARGE);
-    	sizeL.setDefaultPrice(BigInteger.valueOf(180));
-
-    	Set<BouqueteSize> sizes = new HashSet<>();
-    	sizes.add(sizeS);
-    	sizes.add(sizeM);
-    	sizes.add(sizeL);
-    	bouquete.setSizes(sizes);
-    	bouquete.setName("123");
-    	bouquete.setItemCode("BQ051");
-    	bouquete.setQuantity(12);
-    	bouquete.setSoldQuantity(13);
-    	repo.save(bouquete);
+    public Boolean isBouqueteExist(Integer id) {
+        return repo.existsById(id);
     }
 
-	public boolean isBouqueteAvailableForSale(Integer productId) {
-		return repo.isBouqueteAvailableForSale(productId);
-	}
+    public void test() {
+        Bouquete bouquete = new Bouquete();
+        BouqueteSize sizeS = new BouqueteSize();
+        sizeS.setSize(Size.SMALL);
+        sizeS.setDefaultPrice(BigInteger.valueOf(123));
+        BouqueteSize sizeM = new BouqueteSize();
+        sizeM.setSize(Size.MEDIUM);
+        sizeM.setDefaultPrice(BigInteger.valueOf(140));
+        BouqueteSize sizeL = new BouqueteSize();
+        sizeL.setSize(Size.LARGE);
+        sizeL.setDefaultPrice(BigInteger.valueOf(180));
 
-	public Page<Bouquete> getAllBouquetes(Predicate predicate,
-										  Pageable pageable) {
-		return repo.findAll(predicate, pageable);
-	}
+        Set<BouqueteSize> sizes = new HashSet<>();
+        sizes.add(sizeS);
+        sizes.add(sizeM);
+        sizes.add(sizeL);
+        bouquete.setSizes(sizes);
+        bouquete.setName("123");
+        bouquete.setItemCode("BQ051");
+        bouquete.setQuantity(12);
+        bouquete.setSoldQuantity(13);
+        repo.save(bouquete);
+    }
 
-	public Page<Bouquete> searchBouquetsByName(String name, Pageable pageable) {
-		return repo.findByNameContainingIgnoreCase(name, pageable);
-	}
+    public boolean isBouqueteAvailableForSale(Integer productId) {
+        return repo.isBouqueteAvailableForSale(productId);
+    }
+
+    public Page<Bouquete> getAllBouquetes(Predicate predicate,
+                                          Pageable pageable) {
+        return repo.findAll(predicate, pageable);
+    }
+
+    public void deleteImageFromBouquet(Integer bouquetId, Integer imageId) {
+        Bouquete bouquete = repo.findById(bouquetId).orElseThrow();
+        Map<Integer, String> imageUrls = bouquete.getImageUrls();
+        imageUrls.remove(imageId);
+        bouquete.setImageUrls(imageUrls);
+        repo.save(bouquete);
+    }
+
+    public Page<Bouquete> searchBouquetsByName(String name, Pageable pageable) {
+        return repo.findByNameContainingIgnoreCase(name, pageable);
+    }
 }
