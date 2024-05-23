b538671ca7f2944d412d15d31c55af942c223e5d
Serhii Babanov <63935958+SerhiiBabanov@users.noreply.github.com>
2024-05-23 16:07:46 -0400

    change Data annotation to get/set and generate toString and hashcode (#24)

--- src/main/java/ua/flowerista/shop/models/Bouquete.java
+++ src/main/java/ua/flowerista/shop/models/Bouquete.java
@@ -1,21 +1,21 @@
 package ua.flowerista.shop.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.Type;
+import org.hibernate.proxy.HibernateProxy;
+
 import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
-import jakarta.persistence.*;
-import org.hibernate.annotations.Type;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import io.hypersistence.utils.hibernate.type.json.JsonType;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-
-@Data
+@Getter
+@Setter
+@RequiredArgsConstructor
 @ToString
 @Entity
 @Table(name = "bouquete")
@@ -25,7 +25,7 @@ public class Bouquete {
 	@Column(name = "id")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH,
 			CascadeType.REFRESH }, fetch = FetchType.EAGER)
 	@JoinTable(name = "bouquete_flower", joinColumns = @JoinColumn(name = "bouquete_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "flower_id"))
@@ -54,4 +54,19 @@ public class Bouquete {
 	@OneToMany(mappedBy = "bouquete", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
 	private Set<Translate> translates = new LinkedHashSet<>();
 
+	@Override
+	public final boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null) return false;
+		Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+		Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+		if (thisEffectiveClass != oEffectiveClass) return false;
+		Bouquete bouquete = (Bouquete) o;
+		return getId() != null && Objects.equals(getId(), bouquete.getId());
+	}
+
+	@Override
+	public final int hashCode() {
+		return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+	}
 }
