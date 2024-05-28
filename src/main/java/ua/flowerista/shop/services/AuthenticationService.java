1a5a0bc22d62837b420bfb09d077882f6252ec06
Serhii Babanov <serhii.babanov@gmail.com>
2024-05-28 12:48:07 -0400

    disable secure

--- src/main/java/ua/flowerista/shop/services/AuthenticationService.java
+++ src/main/java/ua/flowerista/shop/services/AuthenticationService.java
@@ -106,7 +106,7 @@ public class AuthenticationService {
   private void setRefreshTokenToCookie(HttpServletResponse response, String refreshToken) {
     ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
             .httpOnly(true)
-            .secure(true)
+            .secure(false)
             .path("/api/auth/refresh-token")
             .maxAge(cookieExpiration)
             .build();
