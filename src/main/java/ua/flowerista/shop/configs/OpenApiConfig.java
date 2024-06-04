e5689a96bf655e11e315b15ebcf730bf5985c5af
Serhii Babanov <serhii.babanov@gmail.com>
2024-06-04 17:16:29 -0400

    set access token 15 min, refresh token 24 hours convert properties to yml

--- src/main/java/ua/flowerista/shop/configs/OpenApiConfig.java
+++ src/main/java/ua/flowerista/shop/configs/OpenApiConfig.java
@@ -32,7 +32,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 ),
                 @Server(
                         description = "PROD ENV",
-                        url = "https://floverista-011daa2eb6c3.herokuapp.com/"
+                        url = "https://flowerista-1b251853cf70.herokuapp.com/"
                 )
         },
         security = {
